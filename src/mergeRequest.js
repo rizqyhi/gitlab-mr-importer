1a6405cb8a1d75bc094db9b7a236b0bef913c52d
Rizqy  H <rizqy@dicoding.com>
2022-01-26 18:00:20 +0700

    Format MR response

--- src/mergeRequest.js
+++ src/mergeRequest.js
@@ -1,5 +1,32 @@
 import graphqlClient from "./graphqlClient.js";
 
+function formatMergeRequest(mr) {
+  const reviewers = mr.reviewers.nodes;
+  const notes = mr.notes.nodes.filter((note) => note.system);
+  const startReviewNote = notes.find((note) =>
+    note.body.includes("requested review from")
+  );
+  const reviewRequestedByAuthor = notes.some(
+    (note) =>
+      note.body.includes("requested review from") &&
+      note.author.id === mr.author.id
+  );
+  const hasReviewedByOther =
+    reviewers.length > 0 &&
+    reviewers.some((reviewer) => reviewer.id !== mr.author.id);
+
+  return {
+    ...mr,
+    iid: parseInt(mr.iid),
+    reviewers,
+    notes,
+    systemNotesCount: notes.length,
+    startReviewedAt: startReviewNote ? startReviewNote.createdAt : null,
+    reviewRequestedByAuthor,
+    hasReviewedByOther,
+  };
+}
+
 export async function get(id) {
   const response = await graphqlClient(
     `
@@ -44,5 +71,5 @@ export async function get(id) {
     { id }
   );
 
-  return response.project.mergeRequest;
+  return formatMergeRequest(response.project.mergeRequest);
 }
